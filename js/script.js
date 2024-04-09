ae21665256fe2bd1dad0d1870c757c6494d9065f
andrewsmyth09 <asmyth@gmx.com>
2024-04-09 21:25:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,16 +11,48 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-let quotes = [];
-quotes.push({},{},{},{},{});
-
+let quotes = [
+  {
+    quote: 'May the Force be with you.',
+    source: 'Obi-Wan Kenobi',
+    citation: 'Star Wars: Episode IV - A New Hope',
+    year: 1977
+  },
+  {
+    quote: 'I am your father.',
+    source: 'Darth Vader',
+    citation: 'Star Wars: Episode V - The Empire Strikes Back',
+    year: 1980
+  },
+  {
+    quote: 'Do or do not. There is no try.',
+    source: 'Yoda',
+    citation: 'Star Wars: Episode V - The Empire Strikes Back',
+    year: 1980
+  },
+  {
+    quote: 'Help me, Obi-Wan Kenobi. You\'re my only hope.',
+    source: 'Princess Leia',
+    citation: 'Star Wars: Episode IV - A New Hope',
+    year: 1977
+  },
+  {
+    quote: 'It\'s a trap!',
+    source: 'Admiral Ackbar',
+    citation: 'Star Wars: Episode VI - Return of the Jedi',
+    year: 1983
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
 
-
+const getRandomQuote = () => {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber]['quote'];
+}
 
 /***
  * `printQuote` function
